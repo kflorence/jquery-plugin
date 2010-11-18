16e5b84a3e0b89c0a8d6290f5c90c0441fd4c27c
Kyle Florence <kyle.florence@gmail.com>
2010-11-17 17:48:36 -0800

    even smaller

--- js/jquery.plugin.js
+++ js/jquery.plugin.js
@@ -2,11 +2,12 @@
     $.plugin = function(name, base, prototype) {
         if (!prototype) {
             prototype = base;
-            base = function(options, element) {
+            base = function(element, options) {
                 if (arguments.length) {
                     $.data(element, name, this);
-                    this.element = $(element);
-                    this._init(options);
+                    // ugly workaround for unique option instances -- fixme
+                    this.options = $.extend(true, {}, this.options, options);
+                    this._init();
                 }
             };
 
@@ -16,40 +17,23 @@
         }
 
         $.extend(true, base.prototype, prototype);
-        $.fn[name] = function(options) {
-            var isMethodCall = (typeof options === "string"),
-                args = Array.prototype.slice.call(arguments, 1),
-                returnValue = this; // Chain by default
 
-            if (isMethodCall) {
-                if (options.charAt(0) === "_") { // Psuedo-private
-                    return returnValue;
+        $.fn[name] = function(options) {
+            var result,
+                method = typeof options === "string" ? options : "_init",
+                args = Array.prototype.slice.call(arguments, 1);
+
+            this.each(function() {
+                var instance = $.data(this, name) || new base(this, options);
+
+                if (method.charAt(0) !== "_" && $.isFunction(instance[method])) {
+                    result = instance[method].apply(instance, args);
                 }
 
-                this.each(function() {
-                    var instance = $.data(this, name) || new base({}, this);
+                return result !== undefined;
+            });
 
-                    if (instance && $.isFunction(instance[options])) {
-                        var methodValue = instance[options].apply(instance, args);
-                        if (methodValue !== undefined) {
-                            returnValue = methodValue;
-                            return false;
-                        }
-                    }
-                });
-            } else {
-                this.each(function() {
-                    var instance = $.data(this, name);
-
-                    if (instance) {
-                        instance._init(options);
-                    } else {
-                        new base(options, this);
-                    }
-                });
-            }
-
-            return returnValue;
+            return result !== undefined ? result : this;
         };
     };
 })(jQuery);

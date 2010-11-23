96a24f5129282e56cd16e8a3318ca4444f369cf1
Kyle Florence <kyle.florence@gmail.com>
2010-11-22 18:14:42 -0800

    faster, cleaner; expose base.prototype for future use

--- src/jquery.plugin.js
+++ src/jquery.plugin.js
@@ -2,31 +2,20 @@
   $.plugin = function(name, base, prototype) {
     if (!prototype) {
       prototype = base;
-      base = function(element, options) {
-        if (arguments.length) {
-          $.data(element, name, this);
-          // ugly workaround for unique option instances -- fixme
-          this.options = $.extend(true, {}, this.options, options);
-          this._init();
-        }
-      };
-
-      base.prototype = {
-          _init: function() {}
-      };
+      base = $.plugin.base;
     }
 
     $.extend(true, base.prototype, prototype);
 
     $.fn[name] = function(options) {
       var result,
-        method = typeof options === "string" ? options : "_init",
-        args = Array.prototype.slice.call(arguments, 1);
+        method = typeof options === "string" ? options : "",
+        args = Array.prototype.slice.call(arguments, method ? 1 : 0);
 
       this.each(function() {
-        var instance = $.data(this, name) || new base(this, options);
+        var instance = $.data(this, name) || new base(name, this, args);
 
-        if (method.charAt(0) !== "_" && $.isFunction(instance[method])) {
+        if (method && method.charAt(0) !== "_" && $.isFunction(instance[method])) {
           result = instance[method].apply(instance, args);
         }
 
@@ -35,5 +24,16 @@
 
       return result !== undefined ? result : this;
     };
+  };
+
+  $.plugin.base = function(name, element, args) {
+    if (arguments.length) {
+      $.data(element, name, this);
+      this._init.apply(this, args);
+    }
+  };
+
+  $.plugin.base.prototype = {
+    _init: function() {}
   };
 })(jQuery);
